1b70bb1dcc79c77c57eff1be74e67d4bc1a5705d
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-08-10 16:51:30 +0500

    i add new problems related to function

---
+++ saturday-10-8-24.js
@@ -0,0 +1,24 @@
+// Problem 1
+// Write a function called
+
+function greetUser(){
+    console.log("Hello, Welcome to the class!");
+}
+
+greetUser();
+
+// Problem 2
+
+function displaySquare(x){
+    console.log(x*x);
+}
+
+displaySquare(5);
+
+// Problem 3
+
+function sumOfTwoNumbers(a, b){
+    console.log(a + b);
+}
+
+sumOfTwoNumbers(3, 7);
