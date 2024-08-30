34de74a657604e62d233c0bff2660c3e015af264
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-08-30 08:32:40 +0500

    Updated all  problems

---
+++ switchproblem2.js
@@ -0,0 +1,42 @@
+console.log("switch problem 2");
+
+
+//Problem 1
+
+// var age = 12;
+
+// switch (age){
+//     case 12:
+//         console.log("yes");
+//         break;
+//     default:
+//         console.log("INvalid");    
+// }
+
+
+// var city = "Peshawar";
+
+// switch (true) {
+//     case (city === "Peshawar"):
+//         console.log("I got it" , city)
+//         break;
+//     case "Lahore":
+//         alert("Ohh ");
+//         break;
+//     default:
+//         break;
+// }
+
+var number = 11;
+
+switch (number) {
+    case 12:
+        dozen = true;
+        console.log("Eggs are 1 dozen");
+        break;
+    case 11:
+        dozen = false;    
+        console.log("Eggs are not 1 dozen");
+    default:
+        console.log("Invalid");
+}
